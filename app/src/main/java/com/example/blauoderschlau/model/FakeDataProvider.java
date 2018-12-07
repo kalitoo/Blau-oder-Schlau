c19632945016514fc7fc3d4750e2e9bd2b065f3f
JonasH <jonas.herzog@dlr.de>
2018-12-07 22:00:11 +0100

    added questions

--- app/src/main/java/com/example/blauoderschlau/model/FakeDataProvider.java
+++ app/src/main/java/com/example/blauoderschlau/model/FakeDataProvider.java
@@ -49,19 +49,68 @@ public class FakeDataProvider implements DatabaseManagerContract.Model {
                 new Answer("oben links", false),
                 new Answer("unten rechts", false)};
         QuestionUnit questionUnit4 = new QuestionUnit(q4, ao4);
-        String q5 = "Wer ist der allergeilste?";
+        String q5 = "Wie viele Wörter beinhaltet diese Frage?";
         Answer[] ao5 = new Answer[]{
-                new Answer("Hower", true),
-                new Answer("Hoffmann", false),
-                new Answer("Herzog", false),
-                new Answer("Jost", false)};
+                new Answer("4", false),
+                new Answer("5", false),
+                new Answer("6", true),
+                new Answer("7", false)};
         QuestionUnit questionUnit5 = new QuestionUnit(q5, ao5);
+        String q6 = "Wie viele Plus-Zeichen siehst du?\n++++++";
+        Answer[] ao6 = new Answer[]{
+                new Answer("6", true),
+                new Answer("7", false),
+                new Answer("8", false),
+                new Answer("9", false)};
+        QuestionUnit questionUnit6 = new QuestionUnit(q6, ao6);
+        String q7 = "Wo zeigt die Mehrheit der Pfeile nach rechts?";
+        Answer[] ao7 = new Answer[]{
+                new Answer(">><", true),
+                new Answer("<><", false),
+                new Answer("><<", false),
+                new Answer("AfD", false)};
+        QuestionUnit questionUnit7 = new QuestionUnit(q7, ao7);
+        String q8 = "Wie lustig fandest du die letzte Frage?";
+        Answer[] ao8 = new Answer[]{
+                new Answer("Allerhöchstens Schenkelklopfer", false),
+                new Answer("Ziemlich Arm", false),
+                new Answer("Sehr Bein", false),
+                new Answer("Höchst amüsant", true)};
+        QuestionUnit questionUnit8 = new QuestionUnit(q8, ao8);
+        String q9 = "Wie viel Sterne würdest du dieser App geben?";
+        Answer[] ao9 = new Answer[]{
+                new Answer("Einen", false),
+                new Answer("Zwei", false),
+                new Answer("Drei", false),
+                new Answer("Fünf", true)};
+        QuestionUnit questionUnit9 = new QuestionUnit(q9, ao9);
+        String q10 = "Was ist aus medizinischer Sicht kein Affe?";
+        Answer[] ao10 = new Answer[]{
+                new Answer("Orang-Utan", false),
+                new Answer("Schimpanse", false),
+                new Answer("Kapuzineräffchen", false),
+                new Answer("Oliver Kahn", true)};
+        QuestionUnit questionUnit10 = new QuestionUnit(q10, ao10);
+        String q11 = "Wer sollte sich noch weitere Fragen ausdenken?";
+        Answer[] ao11 = new Answer[]{
+                new Answer("Jan und Jonas", false),
+                new Answer("Jonathan und Jonas", false),
+                new Answer("nur Jonas", false),
+                new Answer("Jan und Jonathan", true)};
+        QuestionUnit questionUnit11 = new QuestionUnit(q11, ao11);
         questionUnits.add(questionUnit0);
         questionUnits.add(questionUnit1);
         questionUnits.add(questionUnit2);
         questionUnits.add(questionUnit3);
         questionUnits.add(questionUnit4);
         questionUnits.add(questionUnit5);
+        questionUnits.add(questionUnit6);
+        questionUnits.add(questionUnit7);
+        questionUnits.add(questionUnit8);
+        questionUnits.add(questionUnit9);
+        questionUnits.add(questionUnit10);
+        questionUnits.add(questionUnit11);
+
 
         for (int i = 0; i < 50; i++) {
             gameHistory.add(new Game(i));
